351b2224e7661771828ec52100baccb47d3b91ab
scauzml <13828607279@163.com>
2019-07-20 20:05:18 +0800

    project init

---
+++ to-list/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <tolist></tolist>
+  </div>
+</template>
+
+<script>
+import tolist from './components/todolist'
+
+export default {
+  name: 'app',
+  components: {
+    tolist
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ to-list/src/components/todolist.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="tolist">
+     {{a}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todolist',
+  props: {
+    msg: String
+  },
+  data(){
+      return {
+       a:"suceess"
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
